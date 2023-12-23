d8fd78cc8e81af74d42114e0fab0df0f1074503d
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-24 01:11:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import { h, render } from "//esm.sh/preact";
 import {
   MiniApp,
   ClosingBehavior,
+  Viewport,
   postEvent
 } from "//cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs";
+
+const viewport = new Viewport();
+viewport.isExpanded = true;
 
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
