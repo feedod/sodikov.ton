953999ac28261ba7e53c5f4efc686307f83bb063
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-24 02:24:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 import { h, render } from "//esm.sh/preact";
-import { init } from "//cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs";
-
-const { mainButton, viewport } = init();
-viewport.expand();
-
-mainButton
-  .setBackgroundColor("#000000")
-  .setTextColor("#ffffff")
-  .setText("Publish")
-  .enable()
-  .show();
 
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
