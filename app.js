b409c197a836e5fdad4e234f7e65a42de75d3e89
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-24 00:50:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,4 @@ import {
 } from "//cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs";
 
 const app = h("h1", null, "Привет, мир!");
-const app += h("h1", null, "Привет, мир!");
-
 render(app, document.body);
