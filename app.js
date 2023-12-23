012abd8d84f727394520287daddfb17743316d1a
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-24 01:14:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import { h, render } from "//esm.sh/preact";
-import {
-  MiniApp,
-  ClosingBehavior,
-  Viewport,
-  postEvent
-} from "//cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs";
+import { init } from "//cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs";
 
-const viewport = new Viewport({
-  isExpanded: true,
-  postEvent
-});
+const { mainButton, viewport } = init();
+
+mainButton.on("click", () => viewport.expand());
+
+mainButton
+  .setBackgroundColor("#000000")
+  .setTextColor("#ffffff")
+  .setText("Expand")
+  .enable()
+  .show();
 
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
