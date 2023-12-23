d5c409051f825a417e3fbd3af0eb0f9ee7a49068
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-23 19:27:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
 import { init } from 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs';
-
-const { mainButton, viewport } = init();
-
-mainButton.on('click', () => viewport.expand());
-
-mainButton
-  .setBackgroundColor('#ffffff')
-  .setTextColor('#000000')
-  .setText('Expand')
-  .enable()
-  .show();
