0afa83e18c9b94f3b9d47a6d1fc94f3dd46f2e05
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-23 23:43:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ import { render } from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
 import { init, MiniApp, ClosingBehavior, postEvent } from 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs';
 
-const { mainButton, viewport } = init();
+const { MiniApp, closingBehaviour, mainButton, viewport } = init();
+
 viewport.expand();
 
-const miniApp = new MiniApp();
 miniApp.setHeaderColor('bg_color');
 
-const closingBehaviour = new ClosingBehavior(false, postEvent);
 closingBehaviour.enableConfirmation();
