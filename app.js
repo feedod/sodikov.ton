2306792f61e574de71d12ba907306dac4c9c532d
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-23 23:34:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-import { h, render } from 'https://esm.sh/preact';
-import { init } from 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs';
+import { render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm';
+import { init, ClosingBehavior, postEvent } from 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@1.2.1/dist/index.mjs';
 
+const { mainButton, viewport } = init();
+viewport.expand();
+
+const closingBehaviour = new ClosingBehavior(false, postEvent);
+closingBehaviour.enableConfirmation();
