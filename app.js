f503b10cd6b6add9cbf0166d495f4553e30c5c50
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-26 19:44:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-import {
-    h,
-    render
-} from "//esm.sh/preact";
+import { h, render } from "//esm.sh/preact";
+
+var charset = document.createElement("meta");
+charset.setAttribute("charset", "utf-8");
+document.getElementsByTagName("head")[0].appendChild(charset);
 
 var viewport = document.createElement("meta");
 viewport.setAttribute("name", "viewport");
 viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
-document.getElementsByTagName("head")[0].appendChild(viewport);
+document.getElementsByTagName("head")[1].appendChild(viewport);
 
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
