366114237514eb95d9722099eb62a87ccb46e053
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-26 19:47:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.getElementsByTagName("head")[0].appendChild(charset);
 var viewport = document.createElement("meta");
 viewport.setAttribute("name", "viewport");
 viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
-document.getElementsByTagName("head")[1].appendChild(viewport);
+document.getElementsByTagName("head")[0].appendChild(viewport);
 
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
