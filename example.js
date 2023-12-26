a04754bd00853cde74e3d7127f893326d0df2df1
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-26 20:34:55 +0300

    Update example.js

--- example.js
+++ example.js
@@ -15,6 +15,25 @@ viewport.setAttribute("name", "viewport");
 viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
 document.getElementsByTagName("head")[0].appendChild(viewport);
 
+
+const twaScript = document.createElement('script');
+twaScript.src = 'https://telegram.org/js/telegram-web-app.js';
+
+twaScript.onload = () => {
+      Telegram.WebApp.expand();
+    
+      var twaViewport = document.createElement("meta");
+      twaViewport.setAttribute("name", "viewport");
+      twaViewport.setAttribute("content", "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no");
+      twaViewport.getElementsByTagName("head")[0].appendChild(twaViewport);
+};
+
+twaScript.onerror = () => {
+  console.log('Error occurred while loading script');
+};
+
+document.body.appendChild(twaScript);
+
 // Content
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
