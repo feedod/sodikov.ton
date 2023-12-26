8a102c2371f4abb63cd5c082e7d902843daf7f61
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-26 20:55:06 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,39 +1,43 @@
 import { h, render } from "//esm.sh/preact";
 
 // html lang
-var lang = navigator.language || navigator.userLanguage;
-document.documentElement.setAttribute("lang", lang);
+setLang(getUserLang());
 
 // Meta Charset
-var charset = document.createElement("meta");
-charset.setAttribute("charset", "utf-8");
-document.getElementsByTagName("head")[0].appendChild(charset);
+setCharset("utf-8");
 
 // Meta Viewport
-var viewport = document.createElement("meta");
-viewport.setAttribute("name", "viewport");
-viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
-document.getElementsByTagName("head")[0].appendChild(viewport);
-
-
-const twaScript = document.createElement('script');
-twaScript.src = 'https://telegram.org/js/telegram-web-app.js';
-
-twaScript.onload = () => {
-      Telegram.WebApp.expand();
-    
-      var twaViewport = document.createElement("meta");
-      twaViewport.setAttribute("name", "viewport");
-      twaViewport.setAttribute("content", "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no");
-      twaViewport.getElementsByTagName("head")[0].appendChild(twaViewport);
-};
-
-twaScript.onerror = () => {
-  console.log('Error occurred while loading script');
-};
-
-document.body.appendChild(twaScript);
+setViewport("width=device-width, initial-scale=1.0");
 
 // Content
 const app = h("h1", null, "Привет, мир!");
 render(app, document.body);
+
+function setLang(content) {
+  document.documentElement.setAttribute("lang", content);
+}
+
+function setCharset(content) {
+  var charset = document.createElement("meta");
+  charset.setAttribute("charset", content);
+  document.getElementsByTagName("head")[0].appendChild(charset);
+}
+
+function setViewport(content) {
+  var viewport = document.createElement("meta");
+  viewport.setAttribute("name", "viewport");
+  viewport.setAttribute("content", content);
+  document.getElementsByTagName("head")[0].appendChild(viewport);
+}
+
+function loadScript(src, onLoadCallback, onErrorCallback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = onLoadCallback;
+  script.onerror = onErrorCallback;
+  document.body.appendChild(script);
+}
+
+function getUserLang() {
+  return navigator.language || navigator.userLanguage;
+}
