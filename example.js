9636640897b779e7df04086e588eab5f2b682a06
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-28 00:04:57 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,13 +1,39 @@
 import Router from 'https://esm.sh/preact-router';
 import { h, render } from 'https://esm.sh/preact';
 
-const Main = () => (
+/** const Main = () => (
   <Router>
     <Home path="/" />
     <About path="/about" />
     // Advanced is an optional query
     <Search path="/search/:query/:advanced?" />
   </Router>
-);
+); **/
 
-render(<Main />, document.body);
+import {h, Component} from 'https://esm.sh/preact';
+import TopAppBar from 'https://esm.sh/preact-material-components/TopAppBar';
+import 'https://esm.sh/preact-material-components/TopAppBar/style.css';
+
+export default class ToolbarPage extends Component {
+  render(){
+    return (
+      <div>
+        <TopAppBar className="topappbar">
+            <TopAppBar.Row>
+              <TopAppBar.Section align-start>
+                <TopAppBar.Icon navigation>menu</TopAppBar.Icon>
+                <TopAppBar.Title>
+                  My App
+                </TopAppBar.Title>
+              </TopAppBar.Section>
+              <TopAppBar.Section align-end>
+                <TopAppBar.Icon>more_vert</TopAppBar.Icon>
+              </TopAppBar.Section>
+            </TopAppBar.Row>
+          </TopAppBar>
+      </div>
+    );
+  }
+}
+
+render(<ToolbarPage />, document.body);
