071fd0f556509f31358e4d930a7ea83fc93f92d7
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-28 01:19:14 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,30 @@
-import { h, render } from 'https://esm.sh/preact';
+import { h, render } from "https://esm.sh/preact";
+
+document.documentElement.setAttribute(
+  "lang",
+  navigator.language || navigator.userLanguage
+);
+
+var charset = document.createElement("meta");
+charset.setAttribute("charset", "utf-8");
+document.getElementsByTagName("head")[0].appendChild(charset);
+
+var viewport = document.createElement("meta");
+viewport.setAttribute("name", "viewport");
+viewport.setAttribute(
+  "content",
+  "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+);
+document.getElementsByTagName("head")[0].appendChild(viewport);
+
+const app = h("h1", null, "Привет, мир!");
+
+render(app, document.body);
+
+function loadScript(src, onLoadCallback, onErrorCallback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = onLoadCallback;
+  script.onerror = onErrorCallback;
+  document.body.appendChild(script);
+}
