5f68031b8d410074ea9642a8c70f7cd60fa7dd14
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-27 23:58:24 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,13 @@
-import { render } from 'https://esm.sh/preact';
+import Router from 'https://esm.sh/preact-router';
+import { h, render } from 'https://esm.sh/preact';
+
+const Main = () => (
+  <Router>
+    <Home path="/" />
+    <About path="/about" />
+    // Advanced is an optional query
+    <Search path="/search/:query/:advanced?" />
+  </Router>
+);
+
+render(<Main />, document.body);
