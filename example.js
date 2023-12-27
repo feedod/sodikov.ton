64666cd0af5f509baf32993573d35b17065a9e5b
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-28 01:32:35 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,14 +1,17 @@
 import { h, render } from "https://esm.sh/preact";
 
+// html lang
 document.documentElement.setAttribute(
   "lang",
   navigator.language || navigator.userLanguage
 );
 
+// meta charset
 var charset = document.createElement("meta");
 charset.setAttribute("charset", "utf-8");
 document.getElementsByTagName("head")[0].appendChild(charset);
 
+// meta viewport
 var viewport = document.createElement("meta");
 viewport.setAttribute("name", "viewport");
 viewport.setAttribute(
@@ -17,9 +20,10 @@ viewport.setAttribute(
 );
 document.getElementsByTagName("head")[0].appendChild(viewport);
 
+// content
 const app = h("h1", null, "Привет, мир!");
-
 render(app, document.body);
+
 
 function loadScript(src, onLoadCallback, onErrorCallback) {
   const script = document.createElement("script");
