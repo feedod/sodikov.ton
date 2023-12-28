c39c058d14ceb44dcb6aa148b02d4e4945bb8180
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-29 00:43:37 +0300

    Update example.js

--- example.js
+++
@@ -1,7 +0,0 @@
-import { render } from "https://jspm.dev/pug@3.0.2";
-
-const head = render(``);
-const body = render(`h1 hello`);
-
-document.head.innerHTML = head;
-document.body.innerHTML = body;
