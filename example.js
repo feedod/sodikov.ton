a434125c8aab7e4ce1630133889a89fdbec6691b
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2023-12-28 23:58:18 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,25 +1,7 @@
-import { h, render } from "https://esm.sh/preact@10.19.3";
+import { render } from 'https://esm.sh/pug@3.0.2';
 
-// html lang
-document.documentElement.setAttribute(
-  "lang",
-  navigator.language || navigator.userLanguage
-);
-
-// meta charset
-var charset = document.createElement("meta");
-charset.setAttribute("charset", "utf-8");
-document.getElementsByTagName("head")[0].appendChild(charset);
-
-// meta viewport
-var viewport = document.createElement("meta");
-viewport.setAttribute("name", "viewport");
-viewport.setAttribute(
-  "content",
-  "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-);
-document.getElementsByTagName("head")[0].appendChild(viewport);
-
-// content
-const app = h("h1", null, "Sodikov");
-render(app, document.body);
+document.body.innerHTML = render(`
+ul
+  - for (let i = 0; i < 100; i++)
+    li #{i}
+`);
