a69a835586badc8af49b7ac1631cd02ce9fb05d2
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-01 19:49:13 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,3 +1,21 @@
 import { marked } from "https://esm.sh/marked@11.1.1";
 
+// html lang
+document.documentElement.setAttribute("lang", "ru");
+
+// meta charset
+var charset = document.createElement("meta");
+charset.setAttribute("charset", "utf-8");
+document.getElementsByTagName("head")[0].appendChild(charset);
+
+// meta viewport
+var viewport = document.createElement("meta");
+viewport.setAttribute("name", "viewport");
+viewport.setAttribute(
+  "content",
+  "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+);
+document.getElementsByTagName("head")[0].appendChild(viewport);
+
+// content
 document.body.innerHTML = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
