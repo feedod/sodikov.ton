2d8538d43dcaa698fae8150dbb7de5a6c27890d6
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-02 01:24:40 +0300

    Update example.js

--- example.js
+++ example.js
@@ -16,7 +16,7 @@ document.head.appendChild(viewportMeta);
 
 // github-markdown-css
 loadStyle(
-  "https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown-dark.css"
+  "https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown.css"
 );
 addStyle(
   "body { background-color: #0d1117; } .markdown-body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; } @media (max-width: 767px) { .markdown-body { padding: 15px; } }"
