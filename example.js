7a13e78dcb11a22eaeda606db90e1c78819c5ec4
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-02 00:57:42 +0300

    Update example.js

--- example.js
+++ example.js
@@ -16,7 +16,7 @@ document.getElementsByTagName("head")[0].appendChild(viewport);
 
 // github-markdown-css
 loadStyle("https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown-dark.css");
-addStyle(".markdown-body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; } @media (max-width: 767px) { .markdown-body { padding: 15px; } }");
+addStyle("body { background-color: #0d1117; } .markdown-body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; } @media (max-width: 767px) { .markdown-body { padding: 15px; } }");
 
 // content
 fetch("https://esm.sh/gh/feedod/sodikov.ton/example.md")
