68d06d14069fcab1addd3bc1548f5cc3ed1eebbf
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-02 01:02:44 +0300

    Update example.js

--- example.js
+++ example.js
@@ -4,15 +4,15 @@ import { marked } from "https://esm.sh/marked@11.1.1";
 document.documentElement.setAttribute("lang", "ru");
 
 // meta charset
-var charset = document.createElement("meta");
-charset.setAttribute("charset", "utf-8");
-document.getElementsByTagName("head")[0].appendChild(charset);
+var charsetMeta = document.createElement("meta");
+charsetMeta.setAttribute("charset", "utf-8");
+document.head.appendChild(charsetMeta);
 
 // meta viewport
-var viewport = document.createElement("meta");
-viewport.setAttribute("name", "viewport");
-viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
-document.getElementsByTagName("head")[0].appendChild(viewport);
+var viewportMeta = document.createElement("meta");
+viewportMeta.setAttribute("name", "viewport");
+viewportMeta.setAttribute("content", "width=device-width, initial-scale=1.0");
+document.head.appendChild(viewportMeta);
 
 // github-markdown-css
 loadStyle("https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown-dark.css");
@@ -25,7 +25,7 @@ fetch("https://esm.sh/gh/feedod/sodikov.ton/example.md")
     var article = document.createElement("article");
     article.classList.add("markdown-body");
     article.innerHTML = marked.parse(md);
-    document.getElementsByTagName("body")[0].appendChild(article);
+    document.body.appendChild(article);
   }).catch((e) => console.error(e));
   
 // title
@@ -35,17 +35,17 @@ function loadStyle(url) {
   var style = document.createElement("link");
   style.rel = "stylesheet";
   style.href = url;
-  document.getElementsByTagName("head")[0].appendChild(style);
+  document.head.appendChild(style);
 }
 
 function addStyle(st) {
   var style = document.createElement("style");
   style.innerHTML = st;
-  document.getElementsByTagName("head")[0].appendChild(style);
+  document.head.appendChild(style);
 }
 
 function loadScript(src) {
   var script = document.createElement("script");
   script.src = src;
-  document.getElementsByTagName("body")[0].appendChild(src);
+  document.body.appendChild(src);
 }
