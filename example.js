2446de8db7474e6925e0f7e7c74a783caf8fd216
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-02 01:06:59 +0300

    Update example.js

--- example.js
+++ example.js
@@ -18,6 +18,12 @@ document.head.appendChild(viewportMeta);
 loadStyle("https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown-dark.css");
 addStyle("body { background-color: #0d1117; } .markdown-body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; } @media (max-width: 767px) { .markdown-body { padding: 15px; } }");
 
+// themecolor Meta
+var themeColorMeta = document.createElement("meta");
+themeColorMeta.setAttribute("name", "theme-color");
+themeColorMeta.setAttribute("content", "#0d1117");
+document.head.appendChild(themeColorMeta);
+
 // content
 fetch("https://esm.sh/gh/feedod/sodikov.ton/example.md")
   .then((res) => res.text())
