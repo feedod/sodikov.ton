9c37781446821b867540e58ce32cc998fe8e45dc
Firdavs Sodikov <154636504+feedod@users.noreply.github.com>
2024-01-02 00:37:03 +0300

    Update example.js

--- example.js
+++ example.js
@@ -14,8 +14,24 @@ viewport.setAttribute("name", "viewport");
 viewport.setAttribute("content", "width=device-width, initial-scale=1.0");
 document.getElementsByTagName("head")[0].appendChild(viewport);
 
+// load github-markdown-css
+loadStyle("https://esm.sh/gh/sindresorhus/github-markdown-css/github-markdown-dark.css");
+
 // content
 fetch("https://esm.sh/gh/feedod/sodikov.ton/example.md")
   .then((res) => res.text())
   .then((md) => document.body.innerHTML = marked.parse(md))
   .catch((e) => console.error(e));
+
+function loadStyle(url) {
+  var style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = url;
+  document.getElementsByTagName("head")[0].appendChild(style);
+}
+
+function loadScript(src) {
+  var script = document.createElement("script");
+  script.src = src;
+  document.getElementsByTagName("body")[0].appendChild(src);
+}
